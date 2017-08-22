d5a880e147b5319e26f308db3627334ee2855ba5
Tommy Tang <twhtanghk@gmail.com>
2017-08-22 08:38:12 +0000

    manual setup default pool after restart libvirtd

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,8 @@ rpc.nfsd
 rpc.mountd
 exportfs -avr
 libvirtd -d
+virsh pool-define-as default dir --target /var/lib/libvirt/images/
+virsh pool-autostart default
+virsh pool-start default
 node_modules/.bin/gulp
 node app.js --prod
