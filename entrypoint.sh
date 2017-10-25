13a1bcef921daadda93e5c0a9a99061e15830377
root <root@f7e07ae816c6>
2017-10-25 14:35:15 +0800

    ignore error if folder /var/lib/libvirt/images exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir /var/lib/libvirt/images
+mkdir -p /var/lib/libvirt/images
 libvirtd -d
 virsh pool-define-as default dir --target /var/lib/libvirt/images/
 virsh pool-autostart default
